444e5cae4fbe78bb17dcc7e00011e5fe0d62c9e9
fe01134 <carlosyells@yahoo.com>
2020-11-04 00:30:24 +0000

    cleaned up and simplified logic using pandas.

--- poc/functions.py
+++ poc/functions.py
@@ -118,11 +118,23 @@ class State_Timer(BaseTransformer):
 
         inputs = []
         inputs.append(ui.UISingleItem(name='state_column', datatype=str, description='Name of column (status)  you want to measure state time in minutes.'))
-        inputs.append(ui.UISingle(name='state_metric_name', datatype=str, description='State name (running) to measure state time in minutes.') )
 
+        #inputs.append(ui.UISingle(name='state_metric_name', datatype=str, description='State name (running) to measure state time in minutes.') )
+        aggregate_names = list(cls.get_available_methods().keys())
+
+        inputs.append(ui.UISingle(name='state_metric_name',
+                               description='Choose states to measure time of',
+                               values=aggregate_names))
 
         # Add output aruguments for each state metric_Name
         outputs = []
         outputs.append(ui.UIFunctionOutSingle(name='state_metric_name', datatype=float, description='Minutes in state'))
 
-        return inputs, outputs
+        return (inputs, outputs)
+
+    @classmethod
+    def get_available_methods(cls):
+        return {
+            'RUNNING': 'RUNNING',
+            'STOPPED': 'STOPPED'
+        }
