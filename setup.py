6d6b37e1a74f5223d0306d3cd18661d26fad3598
Markus Müller <markus_mueller@de.ibm.com>
2021-02-24 11:24:31 +0100

    update

--- setup.py
+++ setup.py
@@ -3,4 +3,4 @@
 from setuptools import setup, find_packages
 
 setup(name='poc', version='0.0.1', packages=find_packages(),
-      install_requires=['mmfunctions>=0.0.2']
+      install_requires=['mmfunctions>=0.0.2'])
